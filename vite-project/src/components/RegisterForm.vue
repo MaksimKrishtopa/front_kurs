d2b3fd07f877f86050168e9d80f8cbd10a4f7c4a
Krishtopa Maksim <k.maks21@mail.ru>
2024-06-17 19:07:02 +0700

    reg design

--- vite-project/src/components/RegisterForm.vue
+++ vite-project/src/components/RegisterForm.vue
@@ -1,21 +1,50 @@
 <template>
+  <div class="container registerform__container">
     <form @submit.prevent="register">
-      <div>
-        <label for="email">Email</label>
+      <h1>Регистрация</h1>
+      <div class="input-group">
+        <label for="surname">Фамилия</label>
+        <input type="text" id="surname" v-model="surname" required />
+      </div>
+      <div class="input-group">
+        <label for="name">Имя</label>
+        <input type="text" id="name" v-model="name" required />
+      </div>
+      <div class="input-group">
+        <label for="patronymic">Отчество</label>
+        <input type="text" id="patronymic" v-model="patronymic" required />
+      </div>
+      <div class="input-group gender_dob">
+        <div>
+        <label for="gender">Пол</label>
+        <select id="gender" v-model="gender" required>
+          <option value="male">Мужской</option>
+          <option value="female">Женский</option>
+        </select>
+      </div>
+        <div>
+        <label for="date_of_birth">Дата рождения</label>
+        <input type="date" id="date_of_birth" v-model="dateOfBirth" required />
+      </div>
+      </div>
+      <div class="input-group">
+        <label for="phone">Телефон</label>
+        <input type="tel" id="phone" v-model="phone" required />
+      </div>
+      <div class="input-group">
+        <label for="email">Почта</label>
         <input type="email" id="email" v-model="email" required />
       </div>
-      <div>
-        <label for="password">Password</label>
+      <div class="input-group">
+        <label for="password">Пароль</label>
         <input type="password" id="password" v-model="password" required />
       </div>
-      <div>
-        <label for="confirmPassword">Confirm Password</label>
-        <input type="password" id="confirmPassword" v-model="confirmPassword" required />
-      </div>
-      <button type="submit">Register</button>
+      <button type="submit">Создать аккаунт</button>
+      <router-link to="/login">Уже есть аккаунт?</router-link>
     </form>
-  </template>
-  
+  </div>
+</template>
+
   <script>
   import { ref } from 'vue'
   import { useRouter } from 'vue-router'
@@ -62,5 +91,90 @@
   </script>
   
   <style scoped>
-  /* Стили для RegisterForm */
+
+  h1 {
+    font-size: 28px;
+    font-weight: 600;
+    padding-bottom: 45px;
+  }
+  
+  .registerform__container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  form {
+    background-color: #239AB5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 45px 75px 110px 75px;
+    border-radius: 10px;
+    gap: 10px;
+  }
+  
+  .input-group {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+
+  input {
+    background-color: #fff;
+    color: #343434;
+    padding: 5px;
+    font-size: 18px;
+    border-radius: 5px;
+  }
+
+  label {
+    font-size: 22px;
+    font-weight: 600;
+    width: 400px;
+  }
+
+  select {
+    background-color: #fff;
+    border-radius: 5px;
+    color: #343434;
+    font-size: 18px;
+    padding: 5px;
+    outline: none;
+  }
+
+  
+  button {
+    background-color: #fff;
+    color: #239AB5;
+    cursor: pointer;
+    padding: 10px 20px;
+    margin-top: 60px;
+    border-radius: 5px;
+    font-weight: 600;
+    font-size: 24px;
+  }
+
+  form > a {
+    padding-top: 10px;
+    color: #fff;
+    font-size: 20px;
+    text-decoration: underline;
+  }
+
+  .gender_dob {
+    display: flex;
+    flex-direction: row;
+    gap: 50px;
+  }
+
+  .gender_dob > div {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .gender_dob label{
+    width: 100%;
+  }
   </style>

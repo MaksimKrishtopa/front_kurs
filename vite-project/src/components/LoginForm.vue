a3bff03f6c783f64c469a465c46a7f57f175c730
Krishtopa Maksim <k.maks21@mail.ru>
2024-06-22 19:08:35 +0700

    дизайн авторизации

--- vite-project/src/components/LoginForm.vue
+++ vite-project/src/components/LoginForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container loginform__container">
     <form @submit.prevent="login">
-      <h1>Авторизация</h1>
+      <h1>Вход</h1>
       <div class="input-group">
         <label for="email">Почта</label>
         <input type="email" id="email" v-model="email" required />
@@ -63,19 +63,66 @@ export default {
 
   <style scoped>
 
-  * {
-    color: #343434;
+
+  .loginform__container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
+
   form {
-    color: #343434;
+    color: #fff;
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
+    background-color: #239AB5;
+    padding: 45px 110px 75px 110px;
+    border-radius: 10px; 
+    
+  }
+
+  label {
+    font-size: 22px;
+    font-weight: 600;
+  }
+
+  input {
+    background-color: #fff;
+    width: 400px;
+    padding: 5px;
+    color: #343434;
+    font-size: 18px;
+    border-radius: 5px;
+  }
+
+  a {
+    padding-top: 5px;
+    color: #fff;
   }
 
   button {
-    background-color: #239AB5;
-    padding: 10px 20px;
+    margin-top: 100px;
+    background-color: #fff;
+    color: #239AB5;
+    padding: 5px 35px;
     cursor: pointer;
+    font-size: 22px;
+    font-weight: 600;
+    border-radius: 5px;
+
+  }
+
+  .input-group {
+    padding-top: 45px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  h1 {
+    font-size: 28px;
+    font-weight: 600;
+    padding-bottom: 35px;
+    color: #fff;
   }
   </style>
